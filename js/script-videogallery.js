0ab9b7f892be30a95bcd5e9ab7fcefe2a541032f
MitchellR0 <123499195+MitchellR0@users.noreply.github.com>
2023-04-29 13:18:52 -0400

    Add files via upload

---
+++ js/script-videogallery.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("cornerBtn").style.display = "block";
+  } else {
+    document.getElementById("cornerBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
